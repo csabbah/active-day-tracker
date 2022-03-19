e542a7fd99583f1a884ae979ab816bffac4a1222
Csabbah <csabbah@ryerson.ca>
2022-03-19 13:45:47 -0400

    Time blocks now have  certain styles based on the time

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,33 @@
 // Use setInerval to check the time and update styling accordingly
-
 // setInterval(() => {
 //   rowHour = $('h5').text();
 //   console.log(rowHour);
 // }, 5000);
 
-$('.hour').each(function (index) {
-  // Extract the time and remove the AM/PM and convert it from string to number
-  hour = parseInt($(this).text().split(' ')[0]);
+// Create a variable to hold the current time (just the hour number)
+var now = moment().format('ha'); // 1pm
 
-  console.log(index, hour);
+// Iterate through all hours....
+$('.hour').each(function () {
+  // Extract the time element value
+  hour = $(this).text().toLowerCase();
+
+  // Referring to the current hour element, extract the description element
+  description = $(this.parentNode).find('.description');
+
+  // Declare specific variables to track current hour element and time
+  var hourEl = moment(hour, 'ha');
+  var currentHour = moment(now, 'ha');
+
+  // Execute condition based on the timing and and add the appropriate classes
+  if (hour == now) {
+    // If hour is the current hour add class of 'present'
+    description.addClass('present');
+  } else if (hourEl.isBefore(currentHour)) {
+    // If the hour is before, add class of 'past'
+    description.addClass('past');
+  } else {
+    // otherwise, add class of 'future'
+    description.addClass('future');
+  }
 });
